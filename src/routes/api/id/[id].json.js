315e8c5dffef8e6f29ff7252c6b9c2266a58fe10
Allan Deutsch <allan@allandeutsch.com>
2022-01-25 21:12:09 -0800

    mocked out student ID json endpoint

--- src/routes/api/id/[id].json.js
+++ src/routes/api/id/[id].json.js
@@ -2,5 +2,21 @@ import { supabase } from '$lib/supabase';
 
 export async function get({ params }) {
 	const { id } = params;
-	supabase.from("students").select();
+	return {
+		body: {
+			"name": "Jane Smith",
+			"id": id,
+			"date": "February 2023",
+			"imageUrl": "https://ca.slack-edge.com/T01QM9YHE80-U02NX39PQ0J-a9d466c6c244-48"
+		}
+	};
+	const { data, error, status } = await supabase
+		.from("students")
+		.select("*")
+		.eq("id", id)
+		.single();
+	if (error && status !== 406) throw error;
+	if (data) {
+		return { body: data };
+	}
 }
